2c292232ee1f0cec0a50096f071eb8fe07ea684b
marutsingh <marut.singh@delhivery.com>
2016-09-21 22:29:21 +0530

    AWS Config

--- ping/src/main/java/com/jspcore/Main.java
+++ ping/src/main/java/com/jspcore/Main.java
@@ -1,5 +1,6 @@
 package com.jspcore;
 
+import com.hazelcast.config.*;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
@@ -16,7 +17,21 @@ public class Main {
     public static void main(final String... args) {
 
 
-        VertxOptions vertxOptions = new VertxOptions().setClustered(true);
+        Config hazelcastConfig = new Config();
+        AwsConfig awsConfig = new AwsConfig();
+        awsConfig.setAccessKey("ACCESS_KEY");
+        awsConfig.setSecretKey("ACCESS_KEY");
+        awsConfig.setRegion("us-west-1");
+
+        hazelcastConfig
+                .setNetworkConfig(new NetworkConfig()
+                .setJoin(new JoinConfig().setMulticastConfig(new MulticastConfig().setEnabled(Boolean.FALSE))
+                        .setAwsConfig(awsConfig)));
+// Now set some stuff on the config (omitted)
+
+        ClusterManager mgr = new HazelcastClusterManager(hazelcastConfig);
+
+        VertxOptions vertxOptions = new VertxOptions().setClustered(true).setClusterManager(mgr);
         System.out.println(args.length);
 
         Vertx.clusteredVertx(vertxOptions, res -> {
